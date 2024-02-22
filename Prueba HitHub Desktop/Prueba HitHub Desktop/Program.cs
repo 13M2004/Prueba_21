d3fbdfbe4f4d8ccce6a99bd7f520886165d7cf57
Alexander Monzon <mmonzonp4@miumg.edu.gt>
2024-02-21 20:15:56 -0600

    Agregar archivos de proyecto.

---
+++ Prueba HitHub Desktop/Prueba HitHub Desktop/Program.cs
@@ -0,0 +1,17 @@
+﻿// Programa_Prueba 
+double numero;
+bool esPar;
+
+Console.WriteLine("Ingresa un número:");
+numero = double.Parse(Console.ReadLine());
+
+esPar = (numero % 2) == 0;
+
+if (esPar)
+{
+    Console.WriteLine("El número es Par: " + numero);
+}
+else
+{
+    Console.WriteLine("El número es Impar: " + numero);
+}
